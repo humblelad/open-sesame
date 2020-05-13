c02209ad0060c10dfe026f4658e4d9515325fe89
humblelad <30574278+humblelad@users.noreply.github.com>
2020-05-13 09:37:22 +0530

    Create test.py

---
+++ archive/test.py
@@ -0,0 +1,28 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+no = 0
+convert = []
+for i in range(0, 429):
+
+    print("Collecting page " + str(i + 1))
+    URL = 'http://h1.nobbd.de/index.php?start=' + str(no)
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(id='reports-all')
+    test = results.find_all(class_='title')
+
+    for o in test:
+        s = o['href']
+        print(s)
+
+        convert.append(s)
+
+    no += 20
+
+with open("copy.txt", "w") as outfile:
+    outfile.write("\n".join(convert))
+
+    # This is the default code used to scrape allt the publlicly disclosed reports.
